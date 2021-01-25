36cbc4a23505e8f26ce8aa1d3c3aeeb7abfcb703
Aaron Nordhoff <aaron@anordhoff.com>
2021-01-24 20:36:03 -0700

    spaces

--- macos/keyboard/options.sh
+++ macos/keyboard/options.sh
@@ -7,7 +7,7 @@
 # right-option -> right-command
 # right-control -> right-option
 hidutil property --set '{"UserKeyMapping":[
-         {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x70000002A},
-         {"HIDKeyboardModifierMappingSrc":0x7000000e6, "HIDKeyboardModifierMappingDst":0x7000000e7},
-         {"HIDKeyboardModifierMappingSrc":0x7000000e4, "HIDKeyboardModifierMappingDst":0x7000000e6}
+        {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x70000002A},
+        {"HIDKeyboardModifierMappingSrc":0x7000000e6, "HIDKeyboardModifierMappingDst":0x7000000e7},
+        {"HIDKeyboardModifierMappingSrc":0x7000000e4, "HIDKeyboardModifierMappingDst":0x7000000e6}
 ]}'
