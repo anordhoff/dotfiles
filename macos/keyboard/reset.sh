a160b7d210dc531036f024a0a5fd72bfd1d08349
Aaron Nordhoff <aaron@anordhoff.com>
2021-01-24 20:36:03 -0700

    spaces

--- macos/keyboard/reset.sh
+++ macos/keyboard/reset.sh
@@ -2,7 +2,7 @@
 
 # reset keyboard mappings to default values
 hidutil property --set '{"UserKeyMapping":[
-         {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000039},
-         {"HIDKeyboardModifierMappingSrc":0x7000000e6, "HIDKeyboardModifierMappingDst":0x7000000e6},
-         {"HIDKeyboardModifierMappingSrc":0x7000000e4, "HIDKeyboardModifierMappingDst":0x7000000e4}
+        {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000039},
+        {"HIDKeyboardModifierMappingSrc":0x7000000e6, "HIDKeyboardModifierMappingDst":0x7000000e6},
+        {"HIDKeyboardModifierMappingSrc":0x7000000e4, "HIDKeyboardModifierMappingDst":0x7000000e4}
 ]}'
