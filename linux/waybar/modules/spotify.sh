eef6c250eb8d20a88add6a977b03c22b98def3c5
Aaron Nordhoff <git@anordhoff.com>
2022-07-23 20:36:44 -0600

    Convert spaces to tabs where necessary

--- linux/waybar/modules/spotify.sh
+++ linux/waybar/modules/spotify.sh
@@ -3,15 +3,15 @@
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
 
 if [[ $class == 'playing' ]]; then
-  info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-  if [[ ${#info} > 80 ]]; then
-    info=$(echo $info | cut -c1-80)"..."
-  fi
-  text="${info}"
+	info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
+	if [[ ${#info} > 80 ]]; then
+		info=$(echo $info | cut -c1-80)"..."
+	fi
+	text="${info}"
 elif [[ $class == 'paused' ]]; then
-  text='Spotify: paused'
+	text='Spotify: paused'
 elif [[ $class == 'stopped' ]]; then
-  text='Spotify: stopped'
+	text='Spotify: stopped'
 fi
 
 echo -e "{\"text\":\"${text}\", \"class\":\"${class}\"}"
