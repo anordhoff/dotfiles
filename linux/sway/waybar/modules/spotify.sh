b7fd94ee24fac46298bbd7b38ea97244637609e5
Aaron Nordhoff <git@anordhoff.com>
2023-01-18 19:28:55 -0700

    Jan 18

---
+++ linux/sway/waybar/modules/spotify.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
+
+if [[ $class == 'playing' ]]; then
+  info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
+  if [[ ${#info} > 80 ]]; then
+    info=$(echo $info | cut -c1-80)"..."
+  fi
+  text=$info
+elif [[ $class == 'paused' ]]; then
+  text='Spotify: paused'
+elif [[ $class == 'stopped' ]]; then
+  text='Spotify: stopped'
+fi
+
+echo -e "{\"text\":\"$text\", \"class\":\"$class\"}"
