ec22d4644c5b5dbd4f94bb885494443521a9bcef
Aaron Nordhoff <git@anordhoff.com>
2023-01-18 19:18:37 -0700

    test

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,6 @@
 user_pref("browser.compactmode.show", true);
+user_pref("browser.gesture.swipe.left", "");
+user_pref("browser.gesture.swipe.right", "");
 user_pref("geo.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("toolkit.scrollbox.verticalScrollDistance", 25);
