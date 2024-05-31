ce2b9c7d5e1776b8933bc03487f5607621dab09a
Aaron Nordhoff <git@anordhoff.com>
2024-05-31 16:09:52 -0600

    lspconfig

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -81,15 +81,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-    -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>rn', function()
-      -- vim.lsp.buf.rename() and print("hi")
-      -- vim.cmd('silent! wa')
-      -- while not vim.lsp.buf.rename() do
-      --   print("hi")
-      -- end
-      vim.lsp.buf.rename()
-    end, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<leader>F', function()
