77f0c1dd6f256edf08d12e4df69683da8f7bdb83
Aaron Nordhoff <git@anordhoff.com>
2025-06-06 18:46:05 -0600

    Remove firefox

--- firefox/user.js
+++
@@ -1,6 +0,0 @@
-user_pref("browser.compactmode.show", true);
-user_pref("browser.gesture.swipe.left", "");
-user_pref("browser.gesture.swipe.right", "");
-user_pref("geo.enabled", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", false);
-user_pref("toolkit.scrollbox.verticalScrollDistance", 25);
