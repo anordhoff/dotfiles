1ea5a2bcc5b3caf7caac5664e93aaa78f7dcaef8
Aaron Nordhoff <git@anordhoff.com>
2025-07-06 21:21:20 -0600

    Add TODO to leap

--- nvim/lua/config/leap.lua
+++ nvim/lua/config/leap.lua
@@ -1,3 +1,7 @@
+-- TODO: https://github.com/ggandor/leap.nvim/issues/243
+--       https://github.com/ggandor/leap.nvim/issues/272
+--       https://github.com/ggandor/leap.nvim/commit/1abca0d146feceeb73679a62271cdedd6a28a8ee
+
 local leap = require('leap')
 
 leap.opts.labels = {'n', 't', 'e', 's', 'i', 'r', 'o', 'a', 'h', 'd', 'u', 'f', 'y', 'w', 'l', 'p', 'm', 'g', '-', 'c', ',', 'x', '\'', 'z', 'k', 'v'}
