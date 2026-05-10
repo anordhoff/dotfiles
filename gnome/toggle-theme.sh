23456cc2ac630a655e4ef3062b872dd1d0dc5849
Aaron Nordhoff <git@anordhoff.com>
2026-05-09 22:29:26 -0600

    Rewrite toggle-theme.sh into a posix compliant script

--- gnome/toggle-theme.sh
+++ gnome/toggle-theme.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $(gsettings get org.gnome.desktop.interface color-scheme) == "'prefer-light'" ]]; then
+if [ $(gsettings get org.gnome.desktop.interface color-scheme) = "'prefer-light'" ]; then
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 else
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
